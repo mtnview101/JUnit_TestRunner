55ea128bb733a27080734d0ed33bc8fb9c7c4b4f
mtnview101 <mtnview101@gmail.com>
2016-12-10 00:04:41 -0800

    third commit

--- A.java
+++ A.java
@@ -4,3 +4,12 @@ public class A{
 		System.out.println(s);
 	}
 }
+/* 
+C:\Runner>javac -cp c:\Runner;junit-4.12.jar;hamcrest-core-1.3.jar;A.java ATest.java TestRunner.java
+
+C:\Runner>java -cp c:\Runner;junit-4.12.jar;hamcrest-core-1.3.jar; TestRunner
+Expected String: Testing Junit 4.12; Actual String: Testing Junit 4.12
+All tests finished successful...
+
+C:\Runner>
+*/
